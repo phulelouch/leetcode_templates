cd62db53ecfb4932715a91b0942857cfb6eadd1a
phulelouch <62769629+phulelouch@users.noreply.github.com>
2024-02-28 19:33:11 +1100

    Update 3.Mono_Stack.py

--- 3.Mono_Stack.py
+++ 3.Mono_Stack.py
@@ -159,3 +159,45 @@ def calculate(input_expression):
 # Example usage:
 result = calculate(["3", "*", "5", "+", "2", "-","3","*","2","/","2"]) 
 print(result)  # Expected output: 15+2-3*2/2
+
+#also Queue template
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Queue:
+    # Implementing this with dummy nodes would be easier!
+    def __init__(self):
+        self.left = self.right = None
+    
+    def enqueue(self, val):
+        newNode = ListNode(val)
+
+        # Queue is non-empty
+        if self.right:
+            self.right.next = newNode
+            self.right = self.right.next
+        # Queue is empty
+        else:
+            self.left = self.right = newNode
+
+    def dequeue(self):
+        # Queue is empty
+        if not self.left:
+            return None
+        
+        # Remove left node and return value
+        val = self.left.val
+        self.left = self.left.next
+        if not self.left:
+            self.right = None
+        return val
+
+    def print(self):
+        cur = self.left
+        while cur:
+            print(cur.val, ' -> ', end ="")
+            cur = cur.next
+        print() # new line
+
