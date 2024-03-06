ac1dc4e1e6ecc29d4551be2d0f6ba290e7ed7f84
phulelouch <62769629+phulelouch@users.noreply.github.com>
2024-03-06 20:05:17 +1100

    Update 3.Mono_Stack.py

--- 3.Mono_Stack.py
+++ 3.Mono_Stack.py
@@ -1,3 +1,18 @@
+#https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+    count = 0
+    while len(students) > count:
+        if students[0] == sandwiches[0]:
+            sandwiches.pop(0)
+            count = 0
+        else:
+            students.append(students[0])
+            count+=1
+
+        students.pop(0)
+    return len(students)
+
+
 # Monostack find bigger on the left
 
 def skyline_stack(nums):
