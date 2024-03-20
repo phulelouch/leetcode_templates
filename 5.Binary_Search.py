d02c7cdfe4d9058c1bd25282c251ccc12c5d185b
phulelouch <62769629+phulelouch@users.noreply.github.com>
2024-03-20 20:20:01 +1100

    Update 5.Binary_Search.py

--- 5.Binary_Search.py
+++ 5.Binary_Search.py
@@ -407,4 +407,61 @@ class Solution:
         # By the end of the while loop, L and R should converge to the minimum element's index
         return nums[L]
         
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+# Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+# Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+# Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+# Example 1:
+
+# Input: piles = [3,6,7,11], h = 8
+# Output: 4
+# Example 2:
+
+# Input: piles = [30,11,23,4,20], h = 5
+# Output: 30
+# Example 3:
+
+# Input: piles = [30,11,23,4,20], h = 6
+# Output: 23
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        piles.sort()
+
+        if len(piles) == h:
+            return max(piles)
+            
+
+        def calHour(speed):
+            hour = 0
+            for pile in piles:
+                hour += (pile + speed - 1) // speed
+            return hour
+
+        L, R = 1, max(piles)+1
+        res = float('inf')
+        while L <= R:
+            M_speed = (L + R) // 2
+            hour = calHour(M_speed)
+            if hour <= h:
+                res = min(res, M_speed)
+                R = M_speed - 1  # Move left if condition is met
+            else:
+                L = M_speed + 1  # Move right otherwise
+                
+        return res
+    
+                
+        return int(res)
+
+       
+
 
